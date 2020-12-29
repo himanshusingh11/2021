39657675309e1e95fedb8e63c51571eab04d31a1
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2020-12-29 14:52:45 +0530

    Just Checking Everything

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int s = x+y;
+        System.out.println(s);
+    }
+}
