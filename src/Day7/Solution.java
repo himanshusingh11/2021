f04e14fef5472ef66bf0fa256698c6a6831a8c63
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2021-06-10 23:54:59 +0530

    Added solution for moving Zeroes

--- src/Day7/Solution.java
+++ src/Day7/Solution.java
@@ -6,6 +6,15 @@ import java.util.Arrays;
 public class Solution {
     public static void main(String[] args) {
         int[] nums = {3,2,0,2,3,5,0};
+        int count = 0;
+        for (int i = 0; i <nums.length ; i++) {
+            if(nums[i]!=0){
+                nums[count++]=nums[i];
+            }
+        }
+        for (int i = count; i <nums.length; i++) {
+            nums[i]=0;
+        }
 
 
         System.out.println(Arrays.toString(nums));
