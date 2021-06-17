13316b75fbcca44edc841d08916edf2c660459dc
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2021-06-17 21:55:31 +0530

    max product subarray

--- src/Day8/Solution.java
+++ src/Day8/Solution.java
@@ -1,4 +1,20 @@
 package Day8;
 
 public class Solution {
+    public static void main(String[] args) {
+        int[] array = {2,3,-2,4};
+        int result = array[0];
+        for (int i =0; i< array.length;i++){
+            int x = array[i];
+
+            for(int j = i+1; j<array.length;j++){
+                result = Math.max(result,x);
+
+                x*=array[j];
+
+            }
+            result = Math.max(result,x);
+        }
+       System.out.println(result);
+    }
 }
