dc090bc75adb507d4d2cedda90558c0617c2e793
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2021-06-18 23:33:38 +0530

    added new class

---
+++ src/Day8/Unknown.java
@@ -0,0 +1,15 @@
+package Day8;
+
+import java.util.Scanner;
+
+public class Unknown {
+    public static void main(String[] args) {
+        
+        // input array
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
+
+
+
