cc5c2a0ee4eb910c50b0525b681a13aea5493cca
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2021-07-01 23:37:14 +0530

    added new class

--- src/Day8/Unknown.java
+++ src/Day8/Unknown.java
@@ -1,15 +1,53 @@
 package Day8;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Unknown {
     public static void main(String[] args) {
-        
-        // input array
-        Scanner scanner = new Scanner(System.in);
 
+
+            Scanner in = new Scanner(System.in);
+            int num = in.nextInt();
+            int tenths = 0;
+            int ones = 0;
+            if(num % 10 != 0){
+                tenths = num - (num % 10);
+                ones = num % 10;
+            }
+            else {
+                ones = num;
+            }
+            int[] tenS = new int[5];
+            int[] oneS = new int[5];
+            if(ones <= 5){
+                while (ones > 0){
+                    oneS[ones-1] = 1;
+                    ones--;
+                }
+            }
+            else {
+                oneS[0] = 1;
+
+            }
+        System.out.println(Arrays.toString(tenS));
+
+        System.out.println(Arrays.toString(oneS));
+           /* int j =0;
+                    int k = 0;
+            int[] finalarray = new int[10];
+        for (int i = 0; i < finalarray.length ; i++) {
+           if(i<=5)
+            finalarray[i]=tenS[i];
+           else{
+               finalarray[i]= oneS[finalarray.length-1];
+           }
+        }
+       System.out.println(Arrays.toString(finalarray));*/
+
+        }
     }
-}
+
 
 
 
---
+++ src/Day9/BitManipulation.java
@@ -0,0 +1,10 @@
+package Day9;
+
+import java.util.Scanner;
+
+public class BitManipulation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
