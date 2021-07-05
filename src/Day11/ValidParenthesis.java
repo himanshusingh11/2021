0c04f23e4ca28e8828520165476129ffff45657e
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2021-07-06 00:10:04 +0530

    added code for valid parenthesis

--- src/Day11/ValidParenthesis.java
+++ src/Day11/ValidParenthesis.java
@@ -1,5 +1,30 @@
 package Day11;
 
-public class ValidParenthesis {
+import java.util.Stack;
 
+public class ValidParenthesis {
+    public static boolean isValid(String s ){
+        Stack<Character>stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c=='('||c=='{'||c=='['){
+                stack.add(c);
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                if(c==']' && stack.peek()!='[') return false;
+                if(c==')' && stack.peek()!='(') return false;
+                if(c=='}' && stack.peek()!='{') return false;
+
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        String s = "()";
+        System.out.println(isValid(s));
+
+    }
 }
